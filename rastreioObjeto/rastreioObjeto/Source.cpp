78dc6dfa85771b25c1b41e84774c7ec709f9c04e
Wellington <wellinlt@gmail.com>
2015-09-29 09:29:46 -0300

    capturando video da webcam do sistema

--- rastreioObjeto/rastreioObjeto/Source.cpp
+++ rastreioObjeto/rastreioObjeto/Source.cpp
@@ -1,17 +1,36 @@
-#include <cstdio>
-#include <opencv2\opencv.hpp>
-#include <opencv2\highgui.hpp>
-#include <opencv\cv.hpp>
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc.hpp"
 using namespace cv;
 
-void main()
-{
-	std::cout << "OpenCV Version: " << CV_VERSION << std::endl;
+//void main()
+//{
+//	std::cout << "OpenCV Version: " << CV_VERSION << std::endl;
+//
+//	//Criar matriz para armazenar imagem
+//	Mat image;
+//	VideoCapture cap;
+//
+//	cap.open(0);
+//
+//}
 
-	//Criar matriz para armazenar imagem
-	Mat image;
-	VideoCapture cap;
+int main(int argc, char** argv) {
 
+
+	cv::VideoCapture cap;
 	cap.open(0);
+	if (!cap.isOpened()){
+		return -1;
+	}
+	cv::Mat frame;
+	cv::namedWindow("Camera", cv::WINDOW_AUTOSIZE);
+	while (1) {
+		cap >> frame;
+		if (!frame.data) break; // Ran out of film
+		cv::imshow("Example3", frame);
+		if (cv::waitKey(33) >= 0) break;
+	}
+	return 0;
 
 }
+		
